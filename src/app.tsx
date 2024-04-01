44e66e93f81ed5f07711cf58eadb279bb7bc00ec
Christian-Schefe <schefechristian@gmail.com>
2024-04-01 21:43:11 +0200

    feat: basic functionality

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,124 @@
 import { useState } from 'preact/hooks';
-import preactLogo from './assets/preact.svg';
-import viteLogo from '/vite.svg';
 import './app.css';
+import { KeyboardListener, ResizeListener } from './keyboard-listener';
+import digitsTxt from '/digits.txt?raw';
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
+    <div class="flex flex-col gap-5">
+      <h1 class="text-2xl">
+        Learn{' '}
+        <span class="[text-shadow:_0px_0px_5px_var(--tw-shadow-color)] shadow-gray-700 rounded-sm font-bold text-xl bg-blue-500 min-w-8 h-8 p-2 inline-flex items-center justify-center">
+          1,000,000
+        </span>{' '}
+        digits of π
+      </h1>
+      <DigitsWrapper></DigitsWrapper>
+      <div class="text-xl">
+        <h1 class="font-bold text-2xl m-3 mt-20"> Controls</h1>
         <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
+          <span class="[text-shadow:_0px_0px_5px_var(--tw-shadow-color)] shadow-gray-700 rounded-sm font-bold text-xl bg-blue-500 min-w-8 h-8 p-2 inline-flex items-center justify-center">
+            R
+          </span>{' '}
+          reset
         </p>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    </div>
+  );
+}
+
+const pi = digitsTxt.slice(2).split('').map(Number);
+
+function DigitsWrapper() {
+  const [digits, setDigits] = useState<number[]>([]);
+  const [mistakes, setMistakes] = useState(0);
+  const [cellsPerRow, setCellsPerRow] = useState(window.outerWidth);
+
+  function handleKey(e: KeyboardEvent) {
+    if (e.key == 'r') return resetProgress();
+
+    const num = parseInt(e.key);
+    if (!Number.isInteger(num) || num < 0 || num >= 10) return;
+    const isCorrect = pi[digits.length] === num;
+    if (!isCorrect) {
+      setMistakes(n => n + 1);
+      return;
+    }
+    setDigits(prev => [...prev, num]);
+  }
+
+  function resetProgress() {
+    setMistakes(0);
+    setDigits(_ => []);
+  }
+
+  function resize(event: Event) {
+    const width = (event.target as Window).outerWidth;
+    setCellsPerRow(_ => Math.min(15, Math.floor(width / 32) - 4));
+    console.log(cellsPerRow)
+  }
+
+  const cells = [];
+  const minRows = 15;
+  const rowsCount = Math.max(
+    Math.floor((digits.length + 1) / cellsPerRow) + 2,
+    minRows,
+  );
+  for (let i = 0; i < rowsCount * cellsPerRow; i++) {
+    if (i == 0) cells.push(DigitCell('3'));
+    else if (i == 1) cells.push(DigitCell('.'));
+    else {
+      const digitIndex = i - 2;
+      const text =
+        digitIndex < digits.length ? digits[digitIndex].toString() : '';
+      cells.push(DigitCell(text));
+    }
+  }
+
+  function getClassByMistakes() {
+    if (mistakes == 0) {
+      return 'bg-green-500';
+    } else if (mistakes <= 3) {
+      return 'bg-yellow-500';
+    } else if (mistakes <= 5) {
+      return 'bg-orange-500';
+    } else {
+      return 'bg-red-500';
+    }
+  }
+
+  const style = {
+    "grid-template-columns": `repeat(${cellsPerRow}, minmax(0, 1fr))`
+  }
+
+  return (
+    <div class="flex flex-col items-center">
+      <KeyboardListener callback={handleKey}></KeyboardListener>
+      <ResizeListener callback={resize}></ResizeListener>
+      <div class="flex flex-col gap-3">
+        <div class="grid grid-cols-3 w-fit gap-[1px] p-[1px] bg-blue-400"
+        style={style}>
+          {cells}
+        </div>
+        <div class="flex gap-2 items-center justify-start col-span-full bg-[#242424]">
+          <p class="text-xl tracking-wide">Mistakes:</p>
+          <div
+            class={`rounded-sm font-bold text-xl ${getClassByMistakes()} min-w-8 h-8 flex items-center justify-center`}
+          >
+            <h1 class="[text-shadow:_0px_0px_5px_var(--tw-shadow-color)] shadow-gray-700">
+              {mistakes}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function DigitCell(text: string) {
+  return (
+    <div class="min-w-8 min-h-8 font-bold flex text-xl items-center justify-center bg-[#242424]">
+      {text}
+    </div>
   );
 }
---
+++ src/keyboard-listener.tsx
@@ -0,0 +1,59 @@
+import { Component } from 'preact';
+
+interface KeyboardListenerProps {
+  callback: (event: KeyboardEvent) => void;
+}
+
+export class KeyboardListener extends Component<KeyboardListenerProps> {
+  constructor(props: KeyboardListenerProps) {
+    super(props);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  keyCallback = () => {};
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    console.log('Key pressed:', event.key);
+    this.props.callback(event);
+  };
+
+  render() {
+    return null;
+  }
+}
+
+interface ResizeListenerProps {
+  callback: (event: Event) => void;
+}
+
+export class ResizeListener extends Component<ResizeListenerProps> {
+  constructor(props: ResizeListenerProps) {
+    super(props);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  keyCallback = () => {};
+
+  handleResize = (event: Event) => {
+    console.log("Resize:", event);
+    this.props.callback(event);
+  };
+
+  render() {
+    return null;
+  }
+}
