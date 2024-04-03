ab6913a684339c2fccf3c6eaa56b07451ba7415a
Christian Schefe <schefe.c@eppendorf.de>
2024-04-03 13:38:07 +0200

    refactor: handling of stored state

--- src/app.tsx
+++ src/app.tsx
@@ -29,34 +29,48 @@ async function saveScore(score: number): Promise<number> {
   return json.id;
 }
 
-export function App() {
-  const [digits, setDigits] = useState(
-    Number.parseInt(localStorage.getItem('digits') ?? '') || 0,
-  );
-  const [mistakes, setMistakes] = useState(
-    Number.parseInt(localStorage.getItem('mistakes') ?? '') || 0,
-  );
-  const [highscore, setHighscore] = useState(
-    Number.parseInt(localStorage.getItem('highscore') ?? '') || 0,
+function useStoredState<T>(
+  key: string,
+  defaultValue: T,
+): [T, (value: T) => void] {
+  const [value, setValue] = useState(
+    JSON.parse(localStorage.getItem(key) ?? 'null') ?? defaultValue,
   );
 
-  const callback = async (digit: number) => {
+  const setAndStoreValue = (newValue: T) => {
+    setValue(newValue);
+    localStorage.setItem(key, JSON.stringify(newValue));
+  };
+
+  return [value, setAndStoreValue];
+}
+
+export function App() {
+  const [digits, setDigits] = useStoredState('digits', 0);
+  const [mistakes, setMistakes] = useStoredState('mistakes', 0);
+  const [highscore, setHighscore] = useStoredState('highscore', 0);
+
+  const onNumberInput = async (digit: number) => {
     const isCorrect = pi[digits] === digit;
     if (!isCorrect) {
-      localStorage.setItem('mistakes', (mistakes + 1).toString());
-      if (mistakes === 0) {
-        console.log("made a mistake, saving score");
+      const newMistakes = mistakes + 1;
+      setMistakes(newMistakes);
+
+      if (newMistakes === 1) {
+        console.log('made a mistake, saving score');
         await saveScore(digits);
       }
-      setMistakes(prev => prev + 1);
     } else {
-      localStorage.setItem('digits', (digits + 1).toString());
-      setDigits(prev => prev + 1);
-      setHighscore(Math.max(highscore, digits + 1));
+      const newDigits = digits + 1;
+      setDigits(newDigits);
+
+      if (mistakes === 0 && newDigits > highscore) {
+        setHighscore(newDigits);
+      }
     }
   };
 
-  const handleKey = (event: KeyboardEvent) => {
+  const onKeyboardInput = (event: KeyboardEvent) => {
     if (event.key === 'r') {
       resetProgress();
       return;
@@ -65,25 +79,23 @@ export function App() {
     const num = parseInt(event.key);
     if (!Number.isInteger(num) || num < 0 || num >= 10) return;
 
-    callback(parseInt(event.key));
+    onNumberInput(parseInt(event.key));
   };
 
   const resetProgress = async () => {
     if (mistakes === 0) {
-      console.log("resetting without a mistake, saving score");
+      console.log('resetting without a mistake, saving score');
       await saveScore(digits);
     }
     setDigits(0);
     setMistakes(0);
-    localStorage.setItem('mistakes', '0');
-    localStorage.setItem('digits', '0');
   };
 
   const digitsToShow = pi.slice(0, digits);
 
   return (
     <div class="flex flex-col gap-5">
-      <KeyboardListener callback={handleKey}></KeyboardListener>
+      <KeyboardListener callback={onKeyboardInput}></KeyboardListener>
       <h1 class="text-2xl">
         Learn{' '}
         <span class="[text-shadow:_0px_0px_5px_var(--tw-shadow-color)] shadow-gray-700 rounded-sm font-bold text-xl bg-blue-500 min-w-8 h-8 p-2 inline-flex items-center justify-center">
@@ -98,7 +110,7 @@ export function App() {
         resetCallback={resetProgress}
       ></DigitsGrid>
       <div class="w-full h-0 mb-4"></div>
-      <DigitButtonRow callback={callback}></DigitButtonRow>
+      <DigitButtonRow callback={onNumberInput}></DigitButtonRow>
     </div>
   );
 }
